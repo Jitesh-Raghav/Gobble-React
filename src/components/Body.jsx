9693eebdb4c793765b00924a30503a66b0e55f1a
Jitesh Kumar Raghav <jkr18042001@gmail.com>
2024-02-04 18:13:15 +0530

    used useEffect hook, added live swiggy api

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,28 @@
 import RestaurantCard from "./RestaurantCard";
 import AllResData from "../utils/mockData";
-import { useState } from "react";                    //react hook is normal js function, like a utility function
-
+import { useState , useEffect } from "react";                    //react hook is normal js function, like a utility function
+                                                                 //useState react hook or a utility function which keeps data and ui layer in sync.
 
 
 const Body= () =>{
 
-    const [listOfRestaurant, setlistOfRestaurant]= useState(AllResData);      //useState react hook or a utility function which keeps data and ui layer in sync.
+    const [listOfRestaurant, setlistOfRestaurant]= useState([]);     //Here, I have removed AllResData from the arg  of useState, cuz now we have live data from api, initially what it was doing that, it was loading the ResCards from allResData then afterwards using the useEffect hook to load the liive data after re-rendering, but now we don't load anything, as we are passing the empty list, so screen reamins white for the time live data loads. In that buffer time we can add a loading animation or shimmer animation..
+
+     useEffect(()=>{
+      fetchData();
+     },[]);
+
+     const fetchData=async ()=>{
+      const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.6148698&lng=73.8729867&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING      ");
+
+      const json= await data.json();
+      console.log(json);
+      setlistOfRestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);  //optionall chaining 
+     };
+
+    if(listOfRestaurant.length==0){
+      return <h1>Loading...</h1>
+    }
 
     return (
        <div className="body">
@@ -19,7 +35,7 @@ const Body= () =>{
 
              }}>Top Rated Restaurants</button>
           </div>
-          
+
            <div className="res-container">
              {listOfRestaurant.map((restaurant)=>(                              //This is basically the iteration of all the restaurants     //study map,filter,reduce...
              <RestaurantCard key={restaurant.info.id} resData={restaurant}/>  //every card should have a unique key, bina uske bhi ho jayega but it should have keys...
